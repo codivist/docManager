8d178f3642d478c1fc7ca4330bebc19417e7c256
Dave Berry <dave@codivist.com>
2022-10-06 21:29:23 -0600

    adding database diagrams

---
+++ Diagrams/docManager.sql
@@ -0,0 +1,24 @@
+CREATE TABLE `files` (
+  `id` bigint(20) PRIMARY KEY AUTO_INCREMENT,
+  `file_name` varchar(100) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `created_at` timestamp,
+  `updated_at` timestamp
+);
+
+CREATE TABLE `versions` (
+  `id` bigint(20) PRIMARY KEY AUTO_INCREMENT,
+  `version_number` integer NOT NULL,
+  `file_id` bigint(20) NOT NULL,
+  `created_at` timestamp,
+  `updated_at` timestamp
+);
+
+CREATE TABLE `tasks` (
+  `id` bigint(20) PRIMARY KEY AUTO_INCREMENT,
+  `task_name` varchar(20) NOT NULL,
+  `file_id` bigint(20) NOT NULL,
+  `is_completed` tinyint(1) NOT NULL,
+  `created_at` timestamp,
+  `updated_at` timestamp
+);
